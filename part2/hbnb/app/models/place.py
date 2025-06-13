313f0cf34b61774a24e57618b4e3e8793b657cf1
Quentin <48597772+loufi84@users.noreply.github.com>
2025-06-13 12:26:31 +0200

    Updated place with methods

--- part2/hbnb/app/models/place.py
+++ part2/hbnb/app/models/place.py
@@ -16,3 +16,31 @@ class Place(BaseModel):
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     updated_at: Optional[datetime] = None
     photos: List[str]
+
+    def set_title(self, title):
+        self.title = title
+        self.updated_at = datetime.now(timezone.utc)
+
+    def set_description(self, description):
+        self.description = description
+        self.updated_at = datetime.now(timezone.utc)
+
+    def set_price(self, price):
+        self.price = price
+        self.updated_at = datetime.now(timezone.utc)
+
+    def set_latitude(self, latitude):
+        self.latitude = latitude
+        self.updated_at = datetime.now(timezone.utc)
+
+    def set_longitude(self, longitude):
+        self.longitude = longitude
+        self.updated_at = datetime.now(timezone.utc)
+
+    def add_photo(self, url):
+        self.photos.append(url)
+        self.updated_at = datetime.now(timezone.utc)
+
+    def remove_photos(self, url):
+        self.photos.remove(url)
+        self.updated_at = datetime.now(timezone.utc)
