54d23a9d4f5f111fdb1fc2b91ea07c05157b8ea0
Quentin <48597772+loufi84@users.noreply.github.com>
2025-06-16 16:54:03 +0200

    Revoilà ta majuscule

--- part2/hbnb/app/api/v1/users.py
+++ part2/hbnb/app/api/v1/users.py
@@ -5,7 +5,7 @@ from pydantic import ValidationError
 from uuid import UUID
 from app.models.user import UserCreate
 
-api = Namespace('users', description='User operations')
+api = Namespace('Users', description='User operations')
 
 # Doc only, no validation here
 user_model = api.model('User', {
