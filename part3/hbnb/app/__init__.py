543394c0f627ee5683be44bca07ee66cf67881ed
Proser-V <10530@hotbertonstudents.com>
2025-06-26 17:13:36 +0200

    db update

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -4,24 +4,28 @@
 '''
 from flask import Flask, redirect
 from flask_restx import Api
-from app.api.v1.users import api as users_ns
-from app.api.v1.amenities import api as amenities_ns
-from app.api.v1.places import api as places_ns
-from app.api.v1.reviews import api as reviews_ns
-from app.api.v1.bookings import api as bookings_ns
 from flask_jwt_extended import JWTManager
 from config import config
 from flask_sqlalchemy import SQLAlchemy
 
 
 db = SQLAlchemy()
+jwt = JWTManager()
 
 def create_app(config_name='default'):
     print("create_app() called")
     app = Flask(__name__)
-    app.config.from_object(config(config_name))
+    app.config.from_object(config[config_name])
     jwt = JWTManager(app)
+
+    from app.api.v1.users import api as users_ns
+    from app.api.v1.amenities import api as amenities_ns
+    from app.api.v1.places import api as places_ns
+    from app.api.v1.reviews import api as reviews_ns
+    from app.api.v1.bookings import api as bookings_ns
+
     db.init_app(app)
+    jwt.init_app(app)
 
     @app.route('/')
     def redirect_to_docs():
