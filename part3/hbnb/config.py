c4d01638a883827de80e64278450098bca09904e
Proser-V <10530@hotbertonstudents.com>
2025-06-26 16:27:27 +0200

    JWT key update in config)

--- part3/hbnb/config.py
+++ part3/hbnb/config.py
@@ -7,7 +7,7 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
-    JWT_SECRET_KEY = 'a-string-secret-at-least-256-bits-long'
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'a-string-secret-at-least-256-bits-long')
     DEBUG = False
 
 
